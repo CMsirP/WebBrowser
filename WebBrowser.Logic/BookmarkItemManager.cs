8b34561c4f00144d723c951a0cb862389d4bb5ca
cmp0106 <cmp0106@DESKTOP-THNH2KU>
2021-02-13 23:18:11 -0500

    Added methods to BookmarkItemManager.

--- WebBrowser.Logic/BookmarkItemManager.cs
+++ WebBrowser.Logic/BookmarkItemManager.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebBrowser.Data.History_BookmarksDataSetTableAdapters;
 
 namespace WebBrowser.Logic
 {
     class BookmarkItemManager
     {
+        public static void AddBookmarkItem(BookmarkItem item)
+        {
+            var adapter = new BookmarksTableAdapter();
+            adapter.Insert(item.URL, item.Title);
+        }
+
+        public static List<BookmarkItem> GetBookmarkItems()
+        {
+            var adapter = new BookmarksTableAdapter();
+            var result = new List<BookmarkItem>();
+            var rows = adapter.GetData();
+            foreach(var row in rows)
+            {
+                result.Add(new BookmarkItem() { Id = row.Id, Title = row.Title, URL = row.URL });
+            }
+            return result;
+        }
     }
 }
