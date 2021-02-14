2d7ad33b8c1ff9825d1a3108ef0ce8042d7c15d9
cmp0106 <cmp0106@DESKTOP-THNH2KU>
2021-02-13 23:22:00 -0500

    Added methods to HistoryItemManager

--- WebBrowser.Logic/HistoryItemManager.cs
+++ WebBrowser.Logic/HistoryItemManager.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebBrowser.Data.History_BookmarksDataSetTableAdapters;
 
 namespace WebBrowser.Logic
 {
     class HistoryItemManager
     {
+        public static void AddHistoryItem(HistoryItem item)
+        {
+            var adapter = new HistoryTableAdapter();
+            adapter.Insert(item.URL, item.Title, item.Date);
+        }
+
+        public static List<HistoryItem> GetHistoryItems()
+        {
+            var adapter = new HistoryTableAdapter();
+            var result = new List<HistoryItem>();
+            var rows = adapter.GetData();
+            foreach (var row in rows)
+            {
+                result.Add(new HistoryItem() { Id = row.Id, Title = row.Title, URL = row.URL, Date = row.Date });
+            }
+            return result;
+        }
     }
 }
